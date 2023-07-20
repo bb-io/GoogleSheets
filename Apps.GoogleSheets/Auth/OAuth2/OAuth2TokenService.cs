d44941dba9fef7d33be8a1f3557417b981f35248
mathijs-bb <mathijs.sonnemans@blackbird.io>
2023-07-20 15:24:28 +0200

    fixed bug on auth connection

--- Apps.GoogleSheets/Auth/OAuth2/OAuth2TokenService.cs
+++ Apps.GoogleSheets/Auth/OAuth2/OAuth2TokenService.cs
@@ -9,7 +9,8 @@ namespace Apps.GoogleSheets.Auth.OAuth2
         private const string ExpiresAtKeyName = "expires_at";
 
         public bool IsRefreshToken(Dictionary<string, string> values)
-            => DateTime.UtcNow > DateTime.Parse(values[ExpiresAtKeyName]);
+            => values.TryGetValue(ExpiresAtKeyName, out var expireValue) &&
+               DateTime.UtcNow > DateTime.Parse(expireValue);
 
         public Task<Dictionary<string, string>> RefreshToken(Dictionary<string, string> values,
             CancellationToken cancellationToken)
@@ -49,46 +50,25 @@ namespace Apps.GoogleSheets.Auth.OAuth2
 
         public Task RevokeToken(Dictionary<string, string> values)
         {
-            var bodyParameters = new Dictionary<string, string>
-            {
-                { "token", values["access_token"] },
-            };
-
-            return SendTokenRequest(Urls.Revoke, bodyParameters, CancellationToken.None);
+            throw new NotImplementedException();
         }
 
         private async Task<Dictionary<string, string>> RequestToken(Dictionary<string, string> bodyParameters,
             CancellationToken cancellationToken)
         {
-            var tokenResponse = await SendTokenRequest(Urls.Token, bodyParameters, cancellationToken);
-            var tokenData = await GetTokenData(tokenResponse);
-
-            var resultDictionary = tokenData
-                .ToDictionary(r => r.Key, r => r.Value?.ToString());
-
+            var utcNow = DateTime.UtcNow;
+            using HttpClient httpClient = new HttpClient();
+            using var httpContent = new FormUrlEncodedContent(bodyParameters);
+            using var response = await httpClient.PostAsync(Urls.Token, httpContent, cancellationToken);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var resultDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(responseContent)
+                                       ?.ToDictionary(r => r.Key, r => r.Value?.ToString())
+                                   ?? throw new InvalidOperationException(
+                                       $"Invalid response content: {responseContent}");
             var expiresIn = int.Parse(resultDictionary["expires_in"]);
-            var expiresAt = DateTime.UtcNow.AddSeconds(expiresIn);
-
+            var expiresAt = utcNow.AddSeconds(expiresIn);
             resultDictionary.Add(ExpiresAtKeyName, expiresAt.ToString());
             return resultDictionary;
-        }
-
-        private async Task<Dictionary<string, object>> GetTokenData(HttpResponseMessage response)
-        {
-            var responseContent = await response.Content.ReadAsStringAsync();
-            return JsonSerializer.Deserialize<Dictionary<string, object>>(responseContent)
-                   ?? throw new InvalidOperationException($"Invalid response content: {responseContent}");
-        }
-
-        private Task<HttpResponseMessage> SendTokenRequest(
-            string url,
-            Dictionary<string, string> bodyParameters,
-            CancellationToken cancellationToken)
-        {
-            using var httpClient = new HttpClient();
-            using var httpContent = new FormUrlEncodedContent(bodyParameters);
-
-            return httpClient.PostAsync(url, httpContent, cancellationToken);
         }
     }
 }
