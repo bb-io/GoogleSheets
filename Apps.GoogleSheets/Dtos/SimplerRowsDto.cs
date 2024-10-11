abe5dbe8c9103eddccf24eeb0d08a18cd6390e24
ce-nistal <cecilia.nistal@blackbird.io>
2024-10-11 12:52:24 -0300

    Row IDs inside row object

---
+++ Apps.GoogleSheets/Dtos/SimplerRowsDto.cs
@@ -0,0 +1,10 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.GoogleSheets.Dtos
+{
+    public class SimplerRowsDto
+    {
+        public List<List<string>> Rows { get; set; }
+
+    }
+}
