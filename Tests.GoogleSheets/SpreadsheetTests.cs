7f3bb2205c2bed91db29437beee4bf9d7dd0e22f
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-18 18:30:50 +0200

    Added unit tests

--- Tests.GoogleSheets/SpreadsheetTests.cs
+++ Tests.GoogleSheets/SpreadsheetTests.cs
@@ -1,14 +1,24 @@
-﻿using Tests.GoogleSheets.Base;
+﻿using Apps.GoogleSheets.Actions;
+using Apps.GoogleSheets.Models.Requests;
+using Tests.GoogleSheets.Base;
 
-namespace Tests.CrowGoogleSheetsdin
+namespace Tests.GoogleSheets
 {
     [TestClass]
     public class SpreadsheetTests : TestBase
     {
         [TestMethod]
-        public async Task Getrange_ReturnsSuccess()
+        public async Task GetRange_ReturnsSuccess()
         {
+            var action = new SpreadsheetActions(InvocationContext, FileManager);
 
+            var spreadsheetFileRequest = new SpreadsheetFileRequest { SpreadSheetId= "" };
+            var spreadSheet = new SheetRequest { SheetName= "YOUR_FILE_NAME" };
+            var rangeRequest = new RangeRequest {StartCell= "A2", EndCell= "C3" };
+
+            var result = await action.GetRange(spreadsheetFileRequest, spreadSheet, rangeRequest);
+
+            Assert.IsNotNull(result);
         }
 
     }
