59b2c7f4ab8e3d45de15ee28292c3ab6458bcc3d
RiabushenkoA <ariabushenko@blackbird.io>
2025-02-18 18:10:23 +0200

    Added unit tests

---
+++ Tests.GoogleSheets/SpreadsheetTests.cs
@@ -0,0 +1,15 @@
+﻿using Tests.GoogleSheets.Base;
+
+namespace Tests.CrowGoogleSheetsdin
+{
+    [TestClass]
+    public class SpreadsheetTests : TestBase
+    {
+        [TestMethod]
+        public async Task Getrange_ReturnsSuccess()
+        {
+
+        }
+
+    }
+}
