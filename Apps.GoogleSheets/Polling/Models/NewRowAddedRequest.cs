ef55b8559097822c1a8798a931b00957da57bc7d
RiabushenkoA <ariabushenko@blackbird.io>
2025-03-03 15:07:17 +0200

    Added new event On new rows added

--- Apps.GoogleSheets/Polling/Models/NewRowAddedRequest.cs
+++ Apps.GoogleSheets/Polling/Models/NewRowAddedRequest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Apps.GoogleSheets.DataSourceHandler;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Polling;
 
 namespace Apps.GoogleSheets.Polling.Models
@@ -11,9 +13,11 @@ namespace Apps.GoogleSheets.Polling.Models
     public class NewRowAddedRequest
     {
         [Display("Spreadsheet ID")]
+        [DataSource(typeof(SpreadsheetFileDataSourceHandler))]
         public string SpreadsheetId { get; set; }
 
         [Display("Sheet name")]
+        [DataSource(typeof(SheetDataSourceHandler))]
         public string SheetName { get; set; }
     }
 }
