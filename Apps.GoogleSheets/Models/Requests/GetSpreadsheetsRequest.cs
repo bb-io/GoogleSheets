d90330945b0c2951b201297ff8d4d98a85b860ff
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-09-26 10:41:05 +0300

    add missing display attribute in GetSpreadsheetsRequest

--- Apps.GoogleSheets/Models/Requests/GetSpreadsheetsRequest.cs
+++ Apps.GoogleSheets/Models/Requests/GetSpreadsheetsRequest.cs
@@ -1,4 +1,5 @@
 ﻿using Apps.GoogleSheets.DataSourceHandler;
+using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.GoogleSheets.Models.Requests;
@@ -6,5 +7,6 @@ namespace Apps.GoogleSheets.Models.Requests;
 public class GetSpreadsheetsRequest
 {
     [DataSource(typeof(FolderDataSourceHandler))]
+    [Display("Folder ID")]
     public string? FolderId { get; set; }
 }
