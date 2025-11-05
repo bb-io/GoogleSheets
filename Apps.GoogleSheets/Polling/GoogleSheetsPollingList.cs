7fe1f648e4ee56dd6df869580964ccae63501e15
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-11-05 18:07:55 +0200

    use file-scoped namespace in updated files

--- Apps.GoogleSheets/Polling/GoogleSheetsPollingList.cs
+++ Apps.GoogleSheets/Polling/GoogleSheetsPollingList.cs
@@ -5,83 +5,82 @@ using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.Sdk.Common.Polling;
 
-namespace Apps.GoogleSheets.Polling
+namespace Apps.GoogleSheets.Polling;
+
+[PollingEventList]
+public class GoogleSheetsPollingList(InvocationContext invocationContext) : BaseInvocable(invocationContext)
 {
-    [PollingEventList]
-    public class GoogleSheetsPollingList(InvocationContext invocationContext) : BaseInvocable(invocationContext)
+    [PollingEvent("On new rows added", "Triggered when new rows are added to the sheet")]
+    public async Task<PollingEventResponse<NewRowAddedMemory, IEnumerable<NewRowResult>>> OnNewRowsAdded(
+         PollingEventRequest<NewRowAddedMemory> request,
+         [PollingEventParameter] SpreadsheetFileRequest spreadsheetFileRequest,
+         [PollingEventParameter] SheetRequest sheetRequest)
     {
-        [PollingEvent("On new rows added", "Triggered when new rows are added to the sheet")]
-        public async Task<PollingEventResponse<NewRowAddedMemory, IEnumerable<NewRowResult>>> OnNewRowsAdded(
-             PollingEventRequest<NewRowAddedMemory> request,
-             [PollingEventParameter] SpreadsheetFileRequest spreadsheetFileRequest,
-             [PollingEventParameter] SheetRequest sheetRequest)
+        var client = new GoogleSheetsClient(InvocationContext.AuthenticationCredentialsProviders);
+
+        var valuesRequest = client.Spreadsheets.Values.Get(spreadsheetFileRequest.SpreadSheetId, sheetRequest.SheetName);
+        var valuesResponse = await ErrorHandler.ExecuteWithErrorHandlingAsync(async () => await valuesRequest.ExecuteAsync());
+
+        int currentRowCount = (valuesResponse.Values != null) ? valuesResponse.Values.Count : 0;
+        if (request.Memory == null)
         {
-            var client = new GoogleSheetsClient(InvocationContext.AuthenticationCredentialsProviders);
-
-            var valuesRequest = client.Spreadsheets.Values.Get(spreadsheetFileRequest.SpreadSheetId, sheetRequest.SheetName);
-            var valuesResponse = await ErrorHandler.ExecuteWithErrorHandlingAsync(async () => await valuesRequest.ExecuteAsync());
-
-            int currentRowCount = (valuesResponse.Values != null) ? valuesResponse.Values.Count : 0;
-            if (request.Memory == null)
+            request.Memory = new NewRowAddedMemory
             {
-                request.Memory = new NewRowAddedMemory
-                {
-                    LastRowCount = currentRowCount,
-                    LastPollingTime = DateTime.UtcNow,
-                    Triggered = false
-                };
-
-                return new PollingEventResponse<NewRowAddedMemory, IEnumerable<NewRowResult>>
-                {
-                    FlyBird = false,
-                    Memory = request.Memory,
-                    Result = null
-                };
-            }
-
-            var memory = request.Memory;
-            var newRowsList = new List<NewRow>();
-
-            if (valuesResponse.Values != null && currentRowCount > memory.LastRowCount)
-            {
-                for (int i = memory.LastRowCount; i < currentRowCount; i++)
-                {
-                    var rowValues = new List<string>();
-                    foreach (var cell in valuesResponse.Values[i])
-                    {
-                        rowValues.Add(cell != null ? cell.ToString() : string.Empty);
-                    }
-
-                    var newRow = new NewRow
-                    {
-                        RowIndex = i + 1,
-                        RowValues = rowValues
-                    };
-                    newRowsList.Add(newRow);
-                }
-            }
-
-            memory.LastRowCount = currentRowCount;
-            memory.LastPollingTime = DateTime.UtcNow;
-            memory.Triggered = newRowsList.Any();
-
-            var result = new List<NewRowResult>();
-            if (newRowsList.Any())
-            {
-                var newRowResult = new NewRowResult
-                {
-                    NewRows = newRowsList
-                };
-                result.Add(newRowResult);
-            }
-
-            var response = new PollingEventResponse<NewRowAddedMemory, IEnumerable<NewRowResult>>
-            {
-                FlyBird = newRowsList.Any(),
-                Memory = memory,
-                Result = result
+                LastRowCount = currentRowCount,
+                LastPollingTime = DateTime.UtcNow,
+                Triggered = false
             };
-            return response;
+
+            return new PollingEventResponse<NewRowAddedMemory, IEnumerable<NewRowResult>>
+            {
+                FlyBird = false,
+                Memory = request.Memory,
+                Result = null
+            };
         }
+
+        var memory = request.Memory;
+        var newRowsList = new List<NewRow>();
+
+        if (valuesResponse.Values != null && currentRowCount > memory.LastRowCount)
+        {
+            for (int i = memory.LastRowCount; i < currentRowCount; i++)
+            {
+                var rowValues = new List<string>();
+                foreach (var cell in valuesResponse.Values[i])
+                {
+                    rowValues.Add(cell != null ? cell.ToString() : string.Empty);
+                }
+
+                var newRow = new NewRow
+                {
+                    RowIndex = i + 1,
+                    RowValues = rowValues
+                };
+                newRowsList.Add(newRow);
+            }
+        }
+
+        memory.LastRowCount = currentRowCount;
+        memory.LastPollingTime = DateTime.UtcNow;
+        memory.Triggered = newRowsList.Any();
+
+        var result = new List<NewRowResult>();
+        if (newRowsList.Any())
+        {
+            var newRowResult = new NewRowResult
+            {
+                NewRows = newRowsList
+            };
+            result.Add(newRowResult);
+        }
+
+        var response = new PollingEventResponse<NewRowAddedMemory, IEnumerable<NewRowResult>>
+        {
+            FlyBird = newRowsList.Any(),
+            Memory = memory,
+            Result = result
+        };
+        return response;
     }
 }
--- Tests.GoogleSheets/PollingTests.cs
+++ Tests.GoogleSheets/PollingTests.cs
@@ -1,92 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Apps.GoogleSheets.Models.Requests;
+﻿using Apps.GoogleSheets.Models.Requests;
 using Apps.GoogleSheets.Polling;
 using Apps.GoogleSheets.Polling.Models;
 using Blackbird.Applications.Sdk.Common.Exceptions;
 using Blackbird.Applications.Sdk.Common.Polling;
 using Tests.GoogleSheets.Base;
 
-namespace Tests.GoogleSheets
+namespace Tests.GoogleSheets;
+
+[TestClass]
+public class PollingTests : TestBase
 {
-    [TestClass]
-    public class PollingTests : TestBase
+    [TestMethod]
+    public async Task Polling_ReturnsSucces()
     {
-        [TestMethod]
-        public async Task Polling_ReturnsSucces()
+        var spreadsheetId = "17ieaCd7SXacxaFr7LkhfdiFRVToyBz1kIzoi6IqM8oc";
+        var sheetName = "Стальна шерсть";
+        int currentRowCount = 6;
+
+        var actions = new GoogleSheetsPollingList(InvocationContext);
+
+        var spreadId = new SpreadsheetFileRequest
         {
-            var spreadsheetId = "17ieaCd7SXacxaFr7LkhfdiFRVToyBz1kIzoi6IqM8oc";
-            var sheetName = "Стальна шерсть";
-            int currentRowCount = 6;
+            SpreadSheetId = spreadsheetId
+        };
+        var spreadName = new SheetRequest
+        {
+            SheetName = sheetName
+        };
 
-            var actions = new GoogleSheetsPollingList(InvocationContext);
+        var pollingRequest = new PollingEventRequest<NewRowAddedMemory>
+        {
+            Memory = new NewRowAddedMemory
+            {
+                LastRowCount = currentRowCount,
+                LastPollingTime = DateTime.UtcNow,
+                Triggered = false
+            }
+        };
 
-            var spreadId = new SpreadsheetFileRequest
-            {
-                SpreadSheetId = spreadsheetId
-            };
-            var spreadName = new SheetRequest
-            {
-                SheetName = sheetName
-            };
+        var response = await actions.OnNewRowsAdded(pollingRequest, spreadId, spreadName);
 
-            var pollingRequest = new PollingEventRequest<NewRowAddedMemory>
+        if (response.Result != null)
+        {
+            foreach (var result in response.Result)
             {
-                Memory = new NewRowAddedMemory
+                foreach (var newRow in result.NewRows)
                 {
-                    LastRowCount = currentRowCount,
-                    LastPollingTime = DateTime.UtcNow,
-                    Triggered = false
-                }
-            };
-
-            var response = await actions.OnNewRowsAdded(pollingRequest, spreadId, spreadName);
-
-            if (response.Result != null)
-            {
-                foreach (var result in response.Result)
-                {
-                    foreach (var newRow in result.NewRows)
+                    Console.WriteLine($"Row Index: {newRow.RowIndex}");
+                    foreach (var cellValue in newRow.RowValues)
                     {
-                        Console.WriteLine($"Row Index: {newRow.RowIndex}");
-                        foreach (var cellValue in newRow.RowValues)
-                        {
-                            Console.WriteLine($"   Cell: {cellValue}");
-                        }
+                        Console.WriteLine($"   Cell: {cellValue}");
                     }
                 }
             }
-
-            Assert.IsNotNull(response, "Response is null.");
         }
 
-        [TestMethod]
-        public async Task OnNewRowsAdded_NonExistingSpreadsheet_ThrowsMisconfigException()
+        Assert.IsNotNull(response, "Response is null.");
+    }
+
+    [TestMethod]
+    public async Task OnNewRowsAdded_NonExistingSpreadsheet_ThrowsMisconfigException()
+    {
+        // Arrange
+        var actions = new GoogleSheetsPollingList(InvocationContext);
+        var spreadId = new SpreadsheetFileRequest { SpreadSheetId = "nonexisting" };
+        var spreadName = new SheetRequest { SheetName = "Стальна шерсть" };
+
+        var pollingRequest = new PollingEventRequest<NewRowAddedMemory>
         {
-            // Arrange
-            var actions = new GoogleSheetsPollingList(InvocationContext);
-            var spreadId = new SpreadsheetFileRequest { SpreadSheetId = "nonexisting" };
-            var spreadName = new SheetRequest { SheetName = "Стальна шерсть" };
-
-            var pollingRequest = new PollingEventRequest<NewRowAddedMemory>
+            Memory = new NewRowAddedMemory
             {
-                Memory = new NewRowAddedMemory
-                {
-                    LastRowCount = 6,
-                    LastPollingTime = DateTime.UtcNow,
-                    Triggered = false
-                }
-            };
-            // Act
-            var ex = await Assert.ThrowsExceptionAsync<PluginMisconfigurationException>(async () => 
-                await actions.OnNewRowsAdded(pollingRequest, spreadId, spreadName)
-            );
+                LastRowCount = 6,
+                LastPollingTime = DateTime.UtcNow,
+                Triggered = false
+            }
+        };
+        // Act
+        var ex = await Assert.ThrowsExceptionAsync<PluginMisconfigurationException>(async () => 
+            await actions.OnNewRowsAdded(pollingRequest, spreadId, spreadName)
+        );
 
-            // Assert
-            StringAssert.Contains(ex.Message, "was not found");
-        }
+        // Assert
+        StringAssert.Contains(ex.Message, "was not found");
     }
 }
