b752d200c0db73ca5f240546d5d3789c8e42e830
Artem Riabushenko <ariabushenko@blackbird.io>
2026-03-12 19:11:17 +0200

    Added actions 'Hide columns' and 'Unhide columns'

---
+++ Apps.GoogleSheets/Models/Requests/HideUnhideColumnsRequest.cs
@@ -0,0 +1,16 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.GoogleSheets.Models.Requests
+{
+    public class HideUnhideColumnsRequest
+    {
+        [Display("All columns", Description = "Hide or unhide all columns in the selected sheet")]
+        public bool? AllColumns { get; set; }
+
+        [Display("Start column", Description = "Column address, e.g. A, B, AA")]
+        public string? StartColumn { get; set; }
+
+        [Display("End column", Description = "Optional end column address, e.g. C, AB")]
+        public string? EndColumn { get; set; }
+    }
+}
